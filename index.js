3e9c979f7df4cb24cb64dca636cc62a3c9c905e2
PaolaHuertaS <paonico11.pnhs@gmail.com>
2023-07-25 19:04:44 -0600

    Segundo video

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const { Client } = require("discord.js");
-const client = new Client ({intents: ["Guilds"]});
+const { Client , GatewayIntentBits, Partials, Collection } = require("discord.js");
+const { Guilds, GuildMembers , GuildMessages } = GatewayIntentBits;
+const { User, Message , GuildMember, ThreadMember } = Partials;
+
+const client = new Client ({intents: [Guilds, GuildMember, GuildMessages], 
+partials: [ User, Message, GuildMember, ThreadMember]
+});
 
 client.config = require("./config.json"); 
+client.events = new Collection();
 console.log(client.config.token);
 client.login(client.config.token).then(() => {
     console.log(`client logged in as ${client.user.username}`);
