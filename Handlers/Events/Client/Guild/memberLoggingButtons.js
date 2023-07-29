611e6b07870393cd31dfd33d72e010c6abfe18e6
PaolaHuertaS <paonico11.pnhs@gmail.com>
2023-07-28 20:07:02 -0600

    Septimo Video

---
+++ Handlers/Events/Client/Guild/memberLoggingButtons.js
@@ -0,0 +1,49 @@
+const { ButtonInteraction, 
+EmbedBuilder} = require("discord.js");
+
+module.exports = {
+    name : "interactionCreate",
+    async execute(interaction){
+        if(!interaction.isButton())return; 
+
+        const splitArray = interaction.customId.split("-");
+        if(!splitArray[0]== "MemberLogging") return;
+        
+        const member = (await interaction.guild.members.fetch()).get(splitArray[2]);
+        const Embed = new EmbedBuilder();
+        const errorArray = [];
+
+        if(!interaction.member.permission.has("KickMembers"))
+        errorArray.push("You do no have the required permissions for this action");
+
+        if(!member)
+        errorArray.push("This user is no loger a member of this guild")
+
+        if(!member.moderatable)
+        errorArray.push(`${member} is not moderatable by this bot`);
+
+        if(errorArray.length) return interaction.reply({
+            embeds: [Embed.setDescription(errorArray.join("\n"))],
+            ephemeral: true
+        });
+
+        switch(splitArray[1]){
+            case "Kick": {
+                member.kick(`Kicked by: ${interaction.user.tag} | Member Logging System`).then(() => {
+                    interaction.reply({ embeds: [Embed.setDescription(`${member} has been kicked.`)]})
+                }).catch(() => {
+                    interaction.reply({ embeds: [Embed.setDescription(`${member} could not be kicked.`)]})
+                })
+            }
+            break;
+            case "Ban":{
+                member.ban(`Banned by: ${interaction.user.tag} | Member Logging System`).then(() => {
+                    interaction.reply({ embeds: [Embed.setDescription(`${member} has been ban.`)]})
+                }).catch(() => {
+                    interaction.reply({ embeds: [Embed.setDescription(`${member} could not be banned.`)]})
+                })
+            }
+            break; 
+        }
+    }
+}
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const { Client , GatewayIntentBits, Partials, Collection } = require("discord.js
 const { Guilds, GuildMembers , GuildMessages } = GatewayIntentBits;
 const { User, Message , GuildMember, ThreadMember } = Partials;
 
-const { loadEvents } = require("./Handlers/eventHandler");
-
-
 const client = new Client ({intents: [Guilds, GuildMember, GuildMessages], 
 partials: [ User, Message, GuildMember, ThreadMember]
 });
@@ -13,11 +10,17 @@ client.config = require("./config.json");
 client.events = new Collection();
 client.subCommands = new Collection();
 client.commands = new Collection();
-loadEvents(client);
+client.guildConfig = new Collection();
+
 const { connect } = require("mongoose");
 connect(client.config.DatabaseURL, {
 }).then(() => console.log("The client is now connected to the databae"));
-console.log(client.config.token)
+
+const{ loadEvents } = require("./Handlers/eventHandler");
+loadEvents(client);
+const { loadConfig } = require("./Functions/configLoader");
+loadConfig(client);
+console.log(client.config.token);
 /*client.login(client.config.token).then(() => {
     console.log(`client logged in as ${client.user.username}`);
     client.user.setActivity(`with ${client.guilds.cache.size} guilds`);
